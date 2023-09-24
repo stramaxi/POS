f588dabc630e0396d7b6db0ed2c044bd18e701ae
stramaxi <maxistranzinger@gmail.com>
2023-09-24 22:23:16 +0200

    Konstrukter

--- Projekt1/src/EratosthenesPrimeSieve.java
+++ Projekt1/src/EratosthenesPrimeSieve.java
@@ -1,2 +1,38 @@
-public class EratosthenesPrimeSieve  {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EratosthenesPrimeSieve  implements PrimeSieve{
+
+    private int bound;
+    private List<Boolean> primes;
+    private List<Integer> numbs;
+
+    public EratosthenesPrimeSieve()
+    {
+        primes = new ArrayList<>();
+        numbs = new ArrayList<>();
+        for (int i = 2; i < bound; i++) {
+
+            numbs.set(i,i);
+            primes.set(i,true);
+
+        }
+
+    }
+    @Override
+    public boolean isPrime(int p) {
+
+        if()
+
+
+
+
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
 }
